4c0ea2f3703b44bb4319adc300124679f7c556a8
sesinos tesfamariam <sesinostesfamariam@sesinoss-mbp.attlocal.net>
2019-08-21 19:03:33 -0700

    Tuesday MVP

---
+++ api/server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session); // remember to pass the session
+
+const authRouter = require('../auth/auth-router.js');
+const usersRouter = require('../users/users-router.js');
+const knexConnection = require('../database/dbConfig.js'); /// <<<<<<<<<
+
+const server = express();
+
+// session configuration
+const sessionOptions = {
+  name: 'fiftyfirstdates',
+  secret: process.env.COOKIE_SECRET || 'keep it secret, keep it safe!', // for encryption
+  cookie: {
+    secure: process.env.COOKIE_SECURE || false, // in production should be true, false for development
+    maxAge: 1000 * 60 * 60 * 24, // how long is the session good for, in milliseconds
+    httpOnly: true, // client JS has no access to the cookie
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new KnexSessionStore({
+    knex: knexConnection,
+    createtable: true,
+    clearInterval: 1000 * 60 * 60, // how long before we clear out expired sessions
+  }),
+};
+
+// global middleware and route handlers
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+server.use(session(sessionOptions));
+
+server.use('/api/auth', authRouter);
+server.use('/api/users', usersRouter);
+
+server.get('/', (req, res) => {
+  res.json({ api: 'up', session: req.session }); // <<<<<<<<<<<
+});
+
+module.exports = server;
--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,24 +1,7 @@
-const bcrypt = require('bcryptjs');
-
-const Users = require('../users/users-model.js');
-
-module.exports = function restricted(req, res, next) {
-  let { username, password } = req.headers;
-
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: 'Invalid Credentials' });
-        }
-      })
-      .catch(error => {
-        res.status(500).json(error);
-      });
+module.exports = (req, res, next) => {
+  if (req.session && req.session.loggedIn) {
+    next();
   } else {
-    res.status(400).json({ message: 'please provide valid credentials' });
+    res.status(401).json({ message: 'you shall not pass!' });
   }
 };
